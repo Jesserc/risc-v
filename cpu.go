ce797971d72c509785e75ac2a0e26559b246df8e
Jesserc <raymondjesse713@gmail.com>
2025-10-27 20:47:49 +0100

    refactor: decode RISC-V instruction fields and implement ADD operation structure

--- cpu.go
+++ cpu.go
@@ -5,9 +5,6 @@ import (
 	"errors"
 	"slices"
 )
-const (
-	testNum uint32 = 0x12345678
-)
 
 type CPU struct {
 	Memory   []byte         // memory is an array of bytes
@@ -58,18 +55,51 @@ func (cpu *CPU) GetRegisterValue(register string) (uint32, error) {
 func (cpu *CPU) FetchAndDecode() (instr uint32, err error) {
 	// fetch instruction from memory
 	// and convert it to a 32-bit word
-	instr = binary.LittleEndian.Uint32(cpu.Memory[cpu.PC : cpu.PC+4]) // this includes the opcode, rs1, rs2, and rd (opcode and operands)
+	instr = binary.LittleEndian.Uint32(cpu.Memory[cpu.PC : cpu.PC+4]) // instr will now be ordered this way: [funct7][rs2][rs1][funct3][rd][opcode]
 
 	cpu.PC += 4 // increment program counter by 4 bytes (32 bits) because each instruction is 4 bytes
 	return instr, nil
 }
 
 func (cpu *CPU) Execute(instr uint32) error {
-	// use a switch statement to determine which instruction to execute based on the opcode
 	// the bit masking (instr & 0x7F) extracts the opcode from the instruction
-	switch instr & 0x7F {
-	case ADD:
-		return cpu.executeAdd(instr)
+	// the bitwise AND op with 0x7F masks out all but the lowest 7 bits of the instruction, which is the opcode
+	// 7 bits because `0x7F` is `0111 1111` - 7 ones and 1 zero
+	// and remember an AND op is a binary operation that takes two operands and returns 1 if both are 1, otherwise 0
+	// so our `instr` which is a 32-bit word (4 bytes) will be masked to only the lowest 7 bits
+	// (according to the risc-v specs, the opcode takes up 7 bits)
+
+	var (
+		// risc-v instructions are ordered this way: [funct7][rs2][rs1][funct3][rd][opcode]...togther they form a 32-bit word
+		// [31:25] funct7 | [24:20] rs2 | [19:15] rs1 | [14:12] funct3 | [11:7] rd | [6:0] opcode
+		// 7 bits for funct7, 5 bits for rs2, 5 bits for rs1, 3 bits for funct3, 5 bits for rd, 7 bits for opcode
+		// so we can extract the funct7, funct3, rs1, rs2, rd and opcode from the instruction by shifting and masking
+
+		opcode = instr & 0x7F         // mask out all but the lowest 7 bits to get the opcode
+		funct3 = (instr >> 12) & 0x7  // shift right by 12 bits and mask out all but the lowest 3 bits to get the funct3 (function code)
+		funct7 = (instr >> 25) & 0x7F // shift right by 25 bits and mask out all but the lowest 7 bits to get the funct7 (function code)
+
+		rs1 = (instr >> 15) & 0x1F // shift right by 15 bits and mask out all but the lowest 5 bits to get the rs1 (source register 1)
+		rs2 = (instr >> 20) & 0x1F // shift right by 20 bits and mask out all but the lowest 5 bits to get the rs2 (source register 2)
+		rd  = (instr >> 7) & 0x1F  // shift right by 7 bits and mask out all but the lowest 5 bits to get the rd (destination register)
+	)
+
+	// use a switch statement to determine which instruction to execute based on the opcode
+	switch opcode {
+	case 0x0:
+	case 0x33: // R-type arithmetic: add, sub, etc (they share the same opcode but are differentiated by funct3 and funct7, which tell the operation variant)
+		switch funct3 {
+		case 0x0:
+			switch funct7 {
+			case 0x00:
+				// add
+				return cpu.executeAdd(instr, rs1, rs2, rd)
+			case 0x20:
+				// sub (unimplemented)
+				return errors.New("sub is unimplemented yet")
+			}
+		}
+
 	case ADDI:
 		return cpu.executeAddi(instr)
 	case LW:
@@ -81,18 +111,34 @@ func (cpu *CPU) Execute(instr uint32) error {
 	default:
 		return errors.New("invalid instruction")
 	}
+
+	return errors.New("unimplemented instruction variant")
 }
 
 // ... follow a fetch-decode-execute cycle ...//
 func (cpu *CPU) Run() {
-
+	for {
+		instr, err := cpu.FetchAndDecode()
+		if err != nil {
+			return
+		}
+		cpu.Execute(instr)
+	}
 }
 
 // Instruction implementations
 
 // ADD
-func (cpu *CPU) executeAdd(instr uint32) error {
-	panic("unimplemented")
+func (cpu *CPU) executeAdd(instr uint32, rs1 uint32, rs2 uint32, rd uint32) error {
+
+	// extract the rs1, rs2, and rd from the instruction
+	// rs1 is bits [19:15]
+
+	// rs2 is bits [24:20]
+
+	// rd is bits [11:7]
+
+	return nil
 }
 
 // ADDI (add immediate - adds a 12-bit immediate value to a register)
